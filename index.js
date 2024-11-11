6d647d032888a583056b2eb067b716d1efcd6742
Alex1234560 <145729384+Alex1234560@users.noreply.github.com>
2024-11-11 10:18:48 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+var canvas = document.querySelector('canvas');
+
+var c = canvas.getContext('2d');
+
+let Var = 0;
+// Va
+
+function update(){
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+
+
+	if (Var < canvas.width - 50){
+	Var+=1;
+	}
+	
+}
+
+	
+
+function draw(){
+	c.fillStyle = "rgb(200 0 0)";
+    c.fillRect(Var, Var/2, 50, 50);
+
+    c.fillStyle = "rgb(0 0 200 / 50%)";
+    c.fillRect(Var, Var, 50, 50);
+	
+}
+
+
+
+function animate() {
+	requestAnimationFrame(animate);
+	
+	update();
+	draw();
+	
+	
+	
+	
+}
+
+animate()
