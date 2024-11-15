aeed84aa8842dc259bccf726fa9da63115d38201
Alex1234560 <145729384+Alex1234560@users.noreply.github.com>
2024-11-14 18:30:32 -0800

    Update index.js

--- index.js
+++ index.js
@@ -6,38 +6,85 @@ var c = canvas.getContext('2d');
 
 const radius = 15; // Radius of the circle
 let BallsOnClick = 10;
+let BounceFactor = 1;//.99;
+let Gravity = 0;//.5;
 
 //let balls = [{"x":30,"y":30,"radius":20,"color":"rgb(0,233,0)","acceleration":acceleration,"angle":30}]//[x,y,radius,color]
 let balls = []
 
-MoreBalls = MakeBalls(0)
+MoreBalls = MakeBalls(20,canvas.width/2, canvas.height/2)
 balls = balls.concat(MoreBalls)
 
-document.addEventListener('click', function(event) {
-	MoreBalls = MakeBalls(BallsOnClick)
+//Remove later 
+
+// balls[0]["angle"] = 0
+// balls[1]["angle"] = 180
+
+// accelerations = splitAcceleration(balls[0]["angle"],balls[0]["acceleration"]);
+// balls[0]["vx"] = accelerations.x
+// balls[0]["vy"] = accelerations.y
+// balls[0]["x"] = 40
+
+// accelerations = splitAcceleration(balls[1]["angle"],balls[1]["acceleration"]);
+// balls[1]["vx"] = accelerations.x
+// balls[1]["vy"] = accelerations.y
+// balls[1]["x"] = 200
+
+//remove later ^^
+
+
+canvas.addEventListener('click', SpawnBalls);
+canvas.addEventListener('touchstart', SpawnBalls);
+
+function SpawnBalls(event) {
+    // Prevent default touch behavior (e.g., scrolling)
+    event.preventDefault();
+
+    const rect = canvas.getBoundingClientRect();
+    let x, y;
+
+    if (event.type === 'touchstart') {
+        // Get touch position from the first touch point
+        x = event.touches[0].clientX - rect.left;
+        y = event.touches[0].clientY - rect.top;
+    } else {
+        // Get mouse click position
+        x = event.clientX - rect.left;
+        y = event.clientY - rect.top;
+    }
+
+	MoreBalls = MakeBalls(BallsOnClick,x,y)
 	balls = balls.concat(MoreBalls);
 
-});
+}
 
-function MakeBalls(num) {
+// document.addEventListener('click', function(event) {
+// 	MoreBalls = MakeBalls(BallsOnClick)
+// 	balls = balls.concat(MoreBalls);
+
+// });
+
+function MakeBalls(num,x,y) {
 
 	array = []
 	for (let i = 0; i < num; i+=1) {
 		dict = {}
-		dict["x"] = canvas.width/2
-		dict["y"] = canvas.height/2
+		dict["x"] = x
+		dict["y"] = y
 		dict["radius"] = radius//getRandomInt(10,10)
 		dict["angle"] = getRandomInt(0,360)
-		dict["acceleration"] = getRandomInt(3,10)
+		dict["acceleration"] = 7//getRandomInt(5,5)
 
 		VariableColor = 255
 		if (balls.length < 255){VariableColor=balls.length}
 		
-		dict["color"] = [VariableColor,getRandomInt(0,255),VariableColor,1]//[getRandomInt(0,255),getRandomInt(0,255),getRandomInt(0,255),.8]
+		dict["color"] = [getRandomInt(0,255),getRandomInt(0,255),getRandomInt(0,255),.8]//[VariableColor,getRandomInt(0,255),VariableColor,1]
 	
 		accelerations = splitAcceleration(dict["angle"],dict["acceleration"]);
 		dict["vx"] = accelerations.x
-		dict["vy"] = accelerations.y
+		dict["vy"] = accelerations.y 
+
+		
 
 		array.push(dict)
 	
@@ -57,25 +104,29 @@ function update(){
 	canvas.height = window.innerHeight;
 
 	for (let i = 0; i < balls.length; i+=1) {
-		if (balls[i]["x"] + radius >= canvas.width || balls[i]["x"]-radius <= 0){ //limits on x axis
-			balls[i]["vx"]*=-1;}
-		if (balls[i]["y"] + radius >= canvas.height || balls[i]["y"]-radius <= 0){ //limits on Y axis
-			balls[i]["vy"]*=-1;
+		ball = balls[i]
 
-		if (balls[i]["y"] + radius >= canvas.height){balls[i]["y"] = canvas.height-radius}//so that ball cant go offscreen when rezising
-		if (balls[i]["x"] + radius >= canvas.width){balls[i]["x"] = canvas.width-radius}//so that ball cant go offscreen when rezising
+		// accelerations = splitAcceleration(ball["angle"],ball["acceleration"]);
+		// ball["vx"] = accelerations.x * Math.sign(ball["vx"])
+		// ball["vy"] = accelerations.y * Math.sign(ball["vy"])
+
+		if (ball["x"] + radius >= canvas.width || ball["x"]-radius <= 0){ //limits on x axis
+			ball["vx"]*=-1;}
+		if (ball["y"] + radius >= canvas.height || ball["y"]-radius <= 0){ //limits on Y axis
+			ball["vy"]*=-BounceFactor;
+
+		if (ball["y"] + radius >= canvas.height){ball["y"] = canvas.height-radius}//so that ball cant go offscreen when rezising
+		if (ball["x"] + radius >= canvas.width){ball["x"] = canvas.width-radius}//so that ball cant go offscreen when rezising
 		}
 
 
-		balls[i]["x"]+=balls[i]["vx"];
-		balls[i]["y"]+=balls[i]["vy"];
+		ball["x"]+=ball["vx"];
+		ball["y"]+=ball["vy"];
+
+		ball["vy"] += Gravity
+		
 		
 	}
-
-	
-	
-
-	
 }
 
 //let bla = `${Math.random() *256},${Math.random() *256},${Math.random() *256}`
